0e3edfe46180242a721feb439212e25f80dd5dd0
rossyspike <jmcamero.24@est.ucab.edu.ve>
2023-12-20 17:18:40 -0400

    Casi listo

--- contraseñas.py
+++ contraseñas.py
@@ -1,44 +1,63 @@
-ubicpatr="patrones"
-ubiccontr="contraseñas"
-tamaño=0
-puntos=0
-formato="{} | {} | {}"
-seguridad=""
-def obt_ubicacion_archivo(ubicacion):
-    print ("Introduzca la ruta del archivo de "+ubicacion+":")
-    ubicacion=str(input(r"ruta:"))
+from os.path import exists as verificar
+
+ubic = ""
+ubiccontr = str
+ruta_contr_nueva = str
+posicion = 0
+puntos = 0
+formato = "{} | {} | {}\n"
+seguridad = str
+contr = str
+
+print("Bienvenido!")
+
+def verif_archivo (archivo):
+    existe = verificar(archivo)
+    if existe != True:
+        print("\nError!! no se encontro el archivo, asegurate de haberlo escrito correctamente.\n")
+    return (existe)#La razon de hacer esta funcion es porque nos parecio un buen toque comprobar si existe el archivo en cuestion, y nos tomamos la libertad de usar la libreria 'os' de python debido a que verificar si existe el archivo no es un requerimiento para el proyecto
+
+def obt_ubicacion_archivo (nombre_archivo,ubicacion,verificar):
+    while True:
+        print ("\nIntroduzca la ruta absoluta del archivo "+nombre_archivo+" *Recuerda incluir la extension del mismo '.txt'")
+        ubicacion=str(input(r"Ruta --:"))
+        if verificar == True:
+            if verif_archivo(ubicacion)==True:
+                break
+        else:
+            break
     return (ubicacion)
 
 def tiene_minusc (palabra,valor):
     for ciclo in range (0,len(palabra)):
-        if palabra[ciclo].islower()==True:
-            valor=+1
+        if palabra[ciclo].islower() == True:
+            valor += 1
             break
-    return(valor)
+    return (valor)
 
 def tiene_mayusc (palabra,valor):
     for ciclo in range (0,len(palabra)):
-        if palabra[ciclo].isupper()==True:
-            valor=+1
+        if palabra[ciclo].isupper() == True:
+            valor += 1
             break
-    return(valor)
+    return (valor)
 
-def tiene_mmro (palabra,valor):
+def tiene_nmro (palabra,valor):
     for ciclo in range (0,len(palabra)):
-        if palabra[ciclo].isnumeric()==True:
-            valor+=1
+        if palabra[ciclo].isnumeric() == True:
+            valor += 1
             break
-    return(valor)
+    return (valor)
 
 def tiene_esp_carac (palabra,valor):
     for ciclo in range (0,len(palabra)):
-        if palabra[ciclo].isnumeric()==False and palabra[ciclo].isalpha()==False:
-            valor+=3
+        if palabra[ciclo].isnumeric() == False and palabra[ciclo].isalpha() == False:
+            valor += 3
             for ciclo in range(ciclo+1,len(palabra)):
-                if palabra[ciclo].isnumeric()==False and palabra[ciclo].isalpha()==False:
-                    valor+=2
+                if palabra[ciclo].isnumeric() == False and palabra[ciclo].isalpha() == False:
+                    valor += 2
             break
-    return(valor)
+    return (valor)
 
 def tiene_patr(palabra,patrones,valor):
     for ciclo in patrones:
@@ -47,28 +66,60 @@ def tiene_patr(palabra,patrones,valor):
             for i in range(0,len(palabra)-1):
                 if len(patron)+i<len(palabra):
                     if patron == palabra[i:len(patron)+i]:
-                        valor+=5                        
+                        valor+=5 
+                           
     return(valor)
 
 def determinar_seguridad (categ,valor):
-    if valor<=15:
-    	categ="débil"
-    elif valor>15 and valor<=20:
-    	categ="moderada"
-    elif valor>20 and valor<=35:
-    	categ="buena"
+    if valor <= 15:
+    	categ = "débil"
+    elif valor > 15 and valor <= 20:
+    	categ = "moderada"
+    elif valor > 20 and valor <= 35:
+    	categ = "buena"
     else:
-    	categ="impenetrable"   
-    return(categ)
+    	categ = "impenetrable"   
+    return (categ)
 
-ubicpatr=obt_ubicacion_archivo(ubicpatr)
-ubiccontr=obt_ubicacion_archivo(ubiccontr)
-ubicpatr=open(ubicpatr)
-ubiccontr=open(ubiccontr)
+def obtener_nro (cadena):
+    for ciclo in range (len(cadena)-3,-1,-1):
+        if cadena[ciclo] == "|":
+            cadena=cadena[ciclo + 1 : len(cadena)]
+            break
+    return int(cadena)
+def tam_lista (archivo):
+    tam = 0
+    with open (archivo,"r") as archivo:
+        for ciclo in archivo:
+            tam += 1
+    lista=[""] * tam
+    return (lista)
 
-for mainloop in ubiccontr:
-    contr=mainloop.strip()
-    puntos=tiene_minusc(contr,puntos)+tiene_mayusc(contr,puntos)+tiene_mmro(contr,puntos)+tiene_esp_carac(contr,puntos)-tiene_patr(contr,ubicpatr,puntos)+len(contr)
-    seguridad=determinar_seguridad(seguridad,puntos)
-    print(formato.format(contr,seguridad,puntos))
-    puntos=0
+ubiccontr = obt_ubicacion_archivo("de contra",ubiccontr,True)
+contrs_calificadas = tam_lista(ubiccontr)
+with open (ubiccontr,"r") as contr_desord:
+   with open (obt_ubicacion_archivo ("de patrones",ubic,True),"r") as ubicpatr:
+       for ciclo in contr_desord:
+            contr=ciclo.strip()
+            puntos=tiene_minusc (contr,puntos) + tiene_mayusc (contr,puntos) + tiene_nmro (contr,puntos) + tiene_esp_carac (contr,puntos) - tiene_patr (contr,ubicpatr,puntos) + len(contr)
+            seguridad=determinar_seguridad (seguridad,puntos)
+            contrs_calificadas[posicion] = contr + " | " + seguridad + " | " + str(puntos)+"\n"
+            puntos=0
+            posicion+=1      
+
+for j in range (0,len(contrs_calificadas)-1):
+    for k in range(0,len(contrs_calificadas)-j-1):
+        posicion = obtener_nro(contrs_calificadas[k])
+        posicion_sig = obtener_nro(contrs_calificadas[k+1])
+        if posicion > posicion_sig:
+            posicion = contrs_calificadas[k+1]
+            contrs_calificadas[k+1] = contrs_calificadas[k]
+            contrs_calificadas[k] = posicion
+
+with open (obt_ubicacion_archivo("donde desea guardar las contrasenas ordenadas",ubic,False),"w") as contr_ordenadas:    
+    for ciclo in range(0,len(contrs_calificadas)):
+        if ciclo != len(contrs_calificadas)-1:
+            contr_ordenadas.write(contrs_calificadas[ciclo])
+        else:
+            contr_ordenadas.write(contrs_calificadas[ciclo].strip("\n"))
+print("Listo! el archivo con las contr ordenadas por nivel de seguridad ha sido exitosamente creado en la ubicacion especificada.")
